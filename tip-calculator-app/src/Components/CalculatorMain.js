dfdaecc4726b5ca57dfa517f2f8809f95a44e632
alnickelzon <tanyaharthart007@gmail.com>
2022-10-08 00:58:19 +0800

    1.0version(done), created gh-page, ready to publish, ready to fix bugs,etc

--- tip-calculator-app/src/Components/CalculatorMain.js
+++ tip-calculator-app/src/Components/CalculatorMain.js
@@ -1,25 +1,35 @@
 import iconperson from '../images/icon-person.svg'
 import icondollar from '../images/icon-dollar.svg'
+import logo from '../images/logo.svg'
 import { useState } from 'react'
 
 const CalculatorMain = () => {
 
-    const amount = [5, 10, 15, 25, 50]
-
     const [tip, setTip] = useState('')
     const [bill, setBill] = useState('')
-    const [customTip] = useState('')
+    const [people, setPeople] = useState(1)
+
+    const [totalTip, setTotalTip] = useState(0)
+
+    var sub = tip / 100
+
+    const reload = () => {
+        window.location.reload()
+    }
 
   return (
     <>
-    <div className="root-container freakin-hate-css">
-        <form className="">
+<div className="root-container">
+<img src={logo} alt={logo} className="logo"/>
+    <div className="main-container freakin-hate-css">
+
+        <form className="form-input">
         <div className="first-section">
             <label htmlFor="bill" className="form-label">Bill</label>
 
             <div className="input-group mb-3">
                 <img src={icondollar} alt={icondollar} className="input-group-text" />
-                <input type="number" id="bill" value={bill} onChange={(e) => setBill(e.target.value)} className="form-control" placeholder="Enter an amount"/>
+                <input type="number" id="bill" value={bill} onChange={(e) => setBill(e.target.value)} className="form-control" placeholder="Enter the amount"/>
             </div>
         </div>
 
@@ -27,12 +37,12 @@ const CalculatorMain = () => {
             <label htmlFor="tip" className="form-label">Select Tip %</label>
 
              <div className="tip-section" id="tip">
-                <button type="button" value={amount[0]} className="buttons" onClick={(e) => setTip(e.target.value)}>5%</button>
-                <button type="button" value={amount[1]} className="buttons" onClick={(e) => setTip(e.target.value)}>10%</button>
-                <button type="button" value={amount[2]} className="buttons" onClick={(e) => setTip(e.target.value)}>15%</button>
-                <button type="button" value={amount[3]} className="buttons" onClick={(e) => setTip(e.target.value)}>25%</button>
-                <button type="button" value={amount[4]} className="buttons" onClick={(e) => setTip(e.target.value)}>50%</button>
-                <input type="number" className="form-control" value={customTip} placeholder="Custom" onChange={(e) => setTip(e.target.value)}></input>
+                <button type="button" className="buttons" onClick={() => setTip(5)}>5%</button>
+                <button type="button" className="buttons" onClick={() => setTip(10)}>10%</button>
+                <button type="button" className="buttons" onClick={() => setTip(15)}>15%</button>
+                <button type="button" className="buttons" onClick={() => setTip(25)}>25%</button>
+                <button type="button" className="buttons" onClick={() => setTip(50)}>50%</button>
+                <input type="number" className="form-control" value={tip} placeholder="How many percent?" onChange={(e) => setTip(e.target.value)}></input>
             </div>
         </div>
 
@@ -41,25 +51,28 @@ const CalculatorMain = () => {
 
             <div className="input-group mb-3">
                 <img src={iconperson} alt={iconperson} className="input-group-text" />
-                <input type="number" id="number-of-people" className="form-control" placeholder="Enter the number of people"/>
+                <input type="number" id="number-of-people" value={people} className="form-control" placeholder="Enter the number of people" onChange={(e) => setPeople(e.target.value)}/>
             </div>
+
+            <button type="button" className="btn btn-warning" onClick={() => setTotalTip((sub * bill))}>Calculate</button>
         </div>
 
         </form>
 
-        <div className="output-section">
+        <form className="output-section">
             <div className="total in-person">
-                <span>Tip Amount<br/>/ person</span>
-                <h3>{tip}</h3>
+                <span>Tip Amount<br/>per person</span>
+                <h3>${(totalTip).toFixed(2)}</h3>
             </div>
             <div className="total all">
-                <span>Total<br/>/ person</span>
-                <h3>{tip}</h3>
+                <span>Total tip:</span>
+                <h3>${(totalTip * people).toFixed(2)}</h3>
             </div>
-            <button type="reset" className="reset">RESET</button>
-        </div>
+            <button type="button" className="reset" onClick={() => reload()}>RESET</button>
+        </form>
 
     </div>
+</div>
     </>
   )
 }
